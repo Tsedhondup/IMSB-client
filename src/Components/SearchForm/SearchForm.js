09e3e785d9fcf554946deaacbacc903aa8f0ab86
Tsering Dhondup <tyddhondup88@gmail.com>
2024-11-17 00:37:59 -0500

    search by sign added

--- src/Components/SearchForm/SearchForm.js
+++ src/Components/SearchForm/SearchForm.js
@@ -3,8 +3,8 @@ import axios from "axios";
 import "./SearchForm.scss";
 const SearchForm = (props) => {
   const [searchValue, setSearchValue] = useState("");
-  const [selectedCondition, setSelectedCondition] = useState("");
-
+  const [selectedCondition, setSelectedCondition] = useState("search by");
+  const [searchByButtonClass, setSearchByButtonClass] = useState("");
   const fitlerConditons = [
     "title",
     "author",
@@ -18,22 +18,27 @@ const SearchForm = (props) => {
   const handleOptionChange = (event) => {
     setSelectedCondition(event.target.value);
   };
-  const handleSearch = () => {
-    axios
-      .get(" http://localhost:8080/filterInventories", {
-        params: {
-          [selectedCondition.toLocaleLowerCase()]: searchValue,
-        },
-      })
-      .then((respond) => {
-        props.setBookLists(respond.data);
-      })
-      .then(() => {
-        props.setHasLoaded(true);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+  const handleSearch = async () => {
+    if (selectedCondition === "search by") {
+      setSearchByButtonClass("js-border-red");
+      return;
+    } else {
+      axios
+        .get(" http://localhost:8080/filterInventories", {
+          params: {
+            [selectedCondition.toLocaleLowerCase()]: searchValue,
+          },
+        })
+        .then((respond) => {
+          props.setBookLists(respond.data);
+        })
+        .then(() => {
+          props.setHasLoaded(true);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
   };
   return (
     <form
@@ -52,13 +57,18 @@ const SearchForm = (props) => {
           handleSearchValue(event);
         }}
       ></input>
+      <h2>Seacrh by</h2>
       <div className="search-filter">
         <select
           className="search-filter__options"
           value={selectedCondition}
           onChange={handleOptionChange}
         >
-          <option className="search-filter__options--item" value="" disabled>
+          <option
+            className={`search-filter__options--item ${searchByButtonClass}`}
+            value=""
+            disabled
+          >
             search by
           </option>
           {fitlerConditons.map((element, index) => (
