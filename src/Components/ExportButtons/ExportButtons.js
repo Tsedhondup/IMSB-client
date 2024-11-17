e4fb60e1b4917748ceecceb51300f643d20cd551
Tsering Dhondup <tyddhondup88@gmail.com>
2024-11-17 01:54:55 -0500

    export buttons component created

---
+++ src/Components/ExportButtons/ExportButtons.js
@@ -0,0 +1,24 @@
+import { exportToJSON } from "../../utilities/exportInJSON";
+import { exportToCSV } from "../../utilities/exportInCSV";
+import "./ExportButtons.scss";
+const ExportButtons = (props) => {
+  return (
+    <div className="export-booklists">
+      <h2 className="exports-booklists__header">Export/Dowload Book lists</h2>
+      <div className="export-booklist__button-container">
+        <button
+          className="export-booklist__button-container--button"
+          onClick={exportToJSON(props.bookLists)}
+        >
+          JSON formate
+        </button>
+        <button
+          className="export-booklist__button-container--button"
+          onClick={exportToCSV(props.bookLists)}
+        >
+          CSV format
+        </button>
+      </div>
+    </div>
+  );
+};
