3554c5d28b6200e63b4eb93799f2cb3cbff8f668
Tsering Dhondup <tyddhondup88@gmail.com>
2024-11-17 08:57:50 -0500

    base styles applied to header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,13 +1,16 @@
 import { useNavigate } from "react-router-dom";
+import "./Header.scss";
 const Headers = () => {
   const navigate = useNavigate();
   const handleAddNew = () => {
     navigate("/addBook");
   };
   return (
-    <div>
-      <h1>Book Inventory</h1>
-      <button onClick={handleAddNew}>add</button>
+    <div className="header">
+      <h1 className="header__site-name">BookInventory</h1>
+      <p onClick={handleAddNew} className="header__add-book-form-link">
+        add book +
+      </p>
     </div>
   );
 };
